b399e45dd4a40182baa6774119297a4f26d3d382
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-03 08:35:59 +0300

    Create Employee.java

---
+++ src/Employee.java
@@ -0,0 +1,13 @@
+public class Employee extends Person{
+
+    //abstract classi miras alan ve methodlari override eden class
+
+    public Employee(String name, int age, int id) {
+        super(name, age, id);
+    }
+
+    @Override
+    public void displayDetails() {
+
+    }
+}
