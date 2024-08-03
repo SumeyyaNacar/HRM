05b115ac35889a53e13e66912a56be6a8a4bae53
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-03 08:36:35 +0300

    Create Manager.java

---
+++ src/Manager.java
@@ -0,0 +1,14 @@
+public class Manager extends Person {
+
+    //abstract classi miras alan ve methodlari override eden class
+    public Manager(String name, int age, int id) {
+        super(name, age, id);
+    }
+
+    @Override
+    public void displayDetails() {
+
+    }
+
+
+}
