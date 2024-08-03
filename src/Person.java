6c59869f5736c3afc807ad064f895784675ea63d
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-03 08:36:53 +0300

    Create Person.java

---
+++ src/Person.java
@@ -0,0 +1,45 @@
+public abstract class Person {
+    //ortak ozelliklerin tanimlandigi class
+
+    private String name;
+    private int age;
+    private int id;
+
+    //constr.
+
+
+    public Person(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    //getter-setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //soyut method
+    public abstract void displayDetails();
+}
