5a8c71bab2857e20cd37ec932218d16776bfccaa
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-04 23:45:41 +0300

    Update PersonService.java

--- src/PersonService.java
+++ src/PersonService.java
@@ -15,46 +15,18 @@ public class PersonService implements IPersonService {
 
     @Override
     public void addPerson(Person person) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Personel turu secin (1-Calisan, 2-Yonetici) : ");
-        int select= scanner.nextInt();
-        System.out.println();
-        System.out.print("Isim : ");
-        String name=scanner.nextLine();
-        System.out.println("Yas : ");
-        int age = scanner.nextInt();
-        System.out.println("ID : ");
-        int id= scanner.nextInt();
-        System.out.println("Maas : ");
-        double salary = scanner.nextInt();
-        if (select==1){
-            Employee employee= new Employee(name,age, id,salary);
-            personService.addPerson(employee);
-        }else {
-            System.out.println("Bolum : ");
-            String departmant = scanner.nextLine();
-            Manager manager = new Manager(name,age,id,salary,departmant);
-            personService.addPerson(manager);
-
-        }
-
-
-
-
-
-
         personList.add(person);
 
     }
 
     @Override
     public void removePerson(int id) {
-        personList.remove(id);
+       personList.removeIf(person -> person.getId()==id);
     }
 
     @Override
-    public List<Person> displayPerson() {
-        return personList;
+    public List<Person> displayAllPerson() {
+       return personList;
     }
 
 }
