29d6447b5478371902aa04c54f81e65ff3e5ba82
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-04 23:02:52 +0300

    Update PersonService.java

--- src/IPersonService.java
+++ src/IPersonService.java
@@ -1,5 +1,11 @@
+import java.util.List;
+
 public interface IPersonService {
     //Personel islemlerini tanimlayan arayuz
+
+    void addPerson();
+    void removePerson();
+    List<Person> displayPerson();
 
 
 }
