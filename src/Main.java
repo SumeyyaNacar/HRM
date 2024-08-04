de2ed9b0b7be98269c4ee7d0552ef88ff3fc8c74
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-08-04 23:45:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) {
         PersonService personService = new PersonService();
         Scanner scanner = new Scanner(System.in);
-        Person person;
+
         start(personService, scanner);
 
     }
 
     private static void start(PersonService personService, Scanner scanner) {
+
         int select;
         do {
             System.out.println("*** Sirket Yonetim Sistemine Hos Geldiniz ***");
@@ -22,8 +23,41 @@ public class Main {
             select = scanner.nextInt();
             switch (select){
                 case 1:
-                    personService.addPerson();
+                    System.out.print("Personel turu secin (1-Calisan, 2-Yonetici) : ");
+                    int selection= scanner.nextInt();
+                    scanner.nextLine();
 
+                    System.out.print("Isim : ");
+                    String name=scanner.nextLine();
+
+                    System.out.println("Yas : ");
+                    int age = scanner.nextInt();
+
+                    System.out.println("ID : ");
+                    int id= scanner.nextInt();
+
+                    System.out.println("Maas : ");
+                    double salary = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (selection==1){
+                        Employee employee= new Employee(name,age, id,salary);
+                        personService.addPerson(employee);
+                    }else {
+                        System.out.println("Bolum : ");
+                        String departmant = scanner.nextLine();
+                        Manager manager = new Manager(name,age,id,salary,departmant);
+                        personService.addPerson(manager);
+
+                    }
+                    break;
+                case 2:
+                  
+                case 3:
+                    for (Person person: personService.displayAllPerson()){
+                        person.displayDetails();
+                    }
+                    break;
             }
 
 
